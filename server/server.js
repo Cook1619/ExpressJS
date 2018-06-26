9002d2de9d429e5fa5782385ba6a60c9305e9139
Cook1619 <cook6348@gmail.com>
2018-06-26 13:09:33 -0500

    Added app.use to log every req.url required

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const path = require('path');
 // app.get('/', (req, res,next) => {
 //     res.send("Hello from the server side...");
 // })
+app.use((req,res,next)=>{
+    console.log(req.originalUrl);
+    next();
+})
 app.use(express.static(path.join(__dirname, '../public')))
 
 
